caf66a600697d4b0bf8d018f477191d64f0a614c
Luis Miguel Zavala Felix <luisbancaprepa@gmail.com>
2022-08-30 16:51:50 -0600

    listen port

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Starting");
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('listening on port: ' + port);
+});
